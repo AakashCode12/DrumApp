b4eb11ce9b9dcb840467d7a735a9435441384143
Shreya Sapale <Shreyasapale@users.noreply.github.com>
2020-11-29 11:22:06 +0530

    Added onClick Listeners

--- app/src/main/java/com/example/drumapp/MainActivity.java
+++ app/src/main/java/com/example/drumapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.drumapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer tom, kick, crash1, crash2,hat,hat2;
+    MediaPlayer tom, kick, crash1, crash2,hat1,hat2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,31 +19,79 @@ public class MainActivity extends AppCompatActivity {
         kick = MediaPlayer.create(MainActivity.this, R.raw.kick);
         crash1 = MediaPlayer.create(MainActivity.this, R.raw.crash1);
         crash2 = MediaPlayer.create(MainActivity.this, R.raw.crash2);
+        hat1 = MediaPlayer.create(MainActivity.this, R.raw.hat);
+        hat2 = MediaPlayer.create(MainActivity.this, R.raw.hat2);
 
-        hat = MediaPlayer.create(MainActivity.this, R.raw.hat);
-        hat2 = MediaPlayer.create(MainActivity.this, R.raw.hat);
+        ImageView leftCrashImg = (ImageView)findViewById(R.id.leftCrash);
+        leftCrashImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crash1.start();
+            }
+        });
+
+        ImageView rightCrashImg = (ImageView)findViewById(R.id.rightCrash);
+        rightCrashImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crash2.start();
+            }
+        });
+
+        ImageView leftHatImg = (ImageView)findViewById(R.id.leftHat);
+        leftHatImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hat1.start();
+            }
+        });
+
+        ImageView rightHatImg = (ImageView)findViewById(R.id.rightHat);
+        rightHatImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hat2.start();
+            }
+        });
+
+        ImageView kickImg = (ImageView)findViewById(R.id.kick);
+        kickImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                kick.start();
+            }
+        });
+
+        ImageView tomsImg = (ImageView)findViewById(R.id.toms);
+        tomsImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tom.start();
+            }
+        });
+
     }
 
-    public void playToms(View view) {
-        tom.start();
-    }
-
-    public void playKick(View view) {
-        kick.start();
-    }
-
-    public void playCrash1(View view) {
-        crash1.start();
-    }
-
-    public void playCrash2(View view) {
-        crash2.start();
-    }
-    public void playHat(View view){
-        hat.start();
-    }
-    public void playHat2(View view){
-        hat2.start();
-    }
+//    public void playToms(View view) {
+//        tom.start();
+//    }
+//
+//    public void playKick(View view) {
+//        kick.start();
+//    }
+//
+//    public void playCrash1(View view) {
+//        crash1.start();
+//    }
+//
+//    public void playCrash2(View view) {
+//        crash2.start();
+//    }
+//    public void playHat(View view){
+//        hat1.start();
+//    }
+//    public void playHat2(View view){
+//        hat2.start();
+//    }
 
 }
