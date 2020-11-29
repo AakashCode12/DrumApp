28ca146b60b40dc684a4276f0ce7f84d2e199dc9
Aakash Yadav <aakashcoder1210@gmail.com>
2020-11-29 11:26:41 +0530

    Made Drum App

--- app/src/main/java/com/example/drumapp/MainActivity.java
+++ app/src/main/java/com/example/drumapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer tom, kick, crash1, crash2,hat,hat2;
+    MediaPlayer tom, kick, crash1, crash2, hat, hat2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         crash2 = MediaPlayer.create(MainActivity.this, R.raw.crash2);
 
         hat = MediaPlayer.create(MainActivity.this, R.raw.hat);
-        hat2 = MediaPlayer.create(MainActivity.this, R.raw.hat);
+        hat2 = MediaPlayer.create(MainActivity.this, R.raw.hat2);
+
     }
 
     public void playToms(View view) {
@@ -37,10 +38,12 @@ public class MainActivity extends AppCompatActivity {
     public void playCrash2(View view) {
         crash2.start();
     }
-    public void playHat(View view){
+
+    public void playHat(View view) {
         hat.start();
     }
-    public void playHat2(View view){
+
+    public void playHat2(View view) {
         hat2.start();
     }
 
